6259a63364f74c88e7ff29a0a828c0c7e5142d9e
ZhangWei-KUMO <kanseefoil@gmail.com>
2021-07-07 22:55:28 +0800

    add new code

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withAntdLess = require('next-plugin-antd-less');
+
+module.exports = withAntdLess({
+  // optional
+  modifyVars: { '@primary-color': '#005cc5' },
+  // optional
+  lessVarsFilePath: './src/styles/variables.less',
+  // optional
+  lessVarsFilePathAppendToEndOfContent: false,
+  // optional https://github.com/webpack-contrib/css-loader#object
+  cssLoaderOptions: {},
+
+  // Other Config Here...
+
+  webpack(config) {
+    return config;
+  }
+});
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,29 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+var parser = require('ua-parser-js');
+var whitelist = ['https://zhangwei-7gl977h782ef503e-1306346100.ap-shanghai.service.tcloudbase.com/rest-api/v1.0/quotation'];
+import Cors from 'cors'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET','HEAD','POST'],
+})
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+      return resolve(result)
+    })
+  })
 }
+
+async function handler(req, res) {
+  // Run the middleware
+  let data = await runMiddleware(req, res, cors)
+  console.log(data)
+  // Rest of the API logic
+  res.json({ message: 'Hello Everyone!' })
+}
+
+export default handler
