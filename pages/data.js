4e11b041c54191957c4b03a9194f3ec64f5fe5cb
ZhangWei-KUMO <kanseefoil@gmail.com>
2021-07-08 16:21:57 +0800

    fixed export bug in data.js

--- pages/data.js
+++ pages/data.js
@@ -1,6 +1,8 @@
-export const likes = [
+ const likes = [
     {id:"2edc5641a20914ada7d2da",name:"赖声川",description:"创意也能有迹可循",types:["生活品味","文化艺术"],cover:"http://dazclassbucket.oss-cn-beijing.aliyuncs.com/uploads/20210701/162513414360dd943fc45d6.jpg"},
     {id:"17b8d75f60f4a7cc2c4d47",name:"曾志伟",description:"一个导演的自我修养",types:["电影电视","文化艺术"],cover:"http://dazclassbucket.oss-cn-beijing.aliyuncs.com/uploads/20210701/162512849660dd7e30e02cd.jpg"},
     {id:"2edc5641a20914ada7d2da",name:"蔡澜",description:"创意也能有迹可循",types:["生活品味","文化艺术"],cover:"http://dazclassbucket.oss-cn-beijing.aliyuncs.com/uploads/20210702/162520477760dea829ce07e.jpg"},
     {id:"2820c6f463db1545cd398b",name:"黄英",description:"用智慧演唱花腔女高音",types:["音乐戏曲","文化艺术"],cover:"http://dazclassbucket.oss-cn-beijing.aliyuncs.com/uploads/20210701/162513559060dd99e6e39e4.jpg"},
 ]
+
+export default likes
