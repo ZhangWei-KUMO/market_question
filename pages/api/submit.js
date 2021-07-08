694e5773369ff1bade9cbcc62bbfe382588d54a6
ZhangWei-KUMO <kanseefoil@gmail.com>
2021-07-08 20:59:31 +0800

    add

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -1,6 +1,5 @@
-var parser = require('ua-parser-js');
-var whitelist = ['https://zhangwei-7gl977h782ef503e-1306346100.ap-shanghai.service.tcloudbase.com/rest-api/v1.0/quotation'];
-const got = require('got');
+import got from 'got'
+import parser from 'ua-parser-js';
 
 const URL = "https://zhangwei-7gl977h782ef503e-1306346100.ap-shanghai.service.tcloudbase.com/rest-api/v1.0/quotation"
 
